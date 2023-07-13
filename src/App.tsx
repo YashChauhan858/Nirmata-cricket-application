13fe83416547224b480d7a05e0d574432e4c27df
Yash Chauhan <yashchauhan858@gmail.com>
2023-07-13 17:37:28 +0530

    Added React router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Component } from '@Components'
+import { Route, Routes } from 'react-router-dom'
+import { Suspense } from 'react'
+/** --------------- @Pages ---------------------- */
 import { Home } from '@Pages'
+/** --------------- @Component ------------------ */
+import { Component } from '@Components'
 
 function App() {
   return (
     <div className="flex h-full">
-      <Component />
-      <Home />
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/player-details" element={<Component />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
