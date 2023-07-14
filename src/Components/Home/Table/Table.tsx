14632a85e27eb373f32293494782fbf7b07009bf
Yash Chauhan <yashchauhan858@gmail.com>
2023-07-14 09:26:41 +0530

    sort functionality added

--- src/Components/Home/Table/Table.tsx
+++ src/Components/Home/Table/Table.tsx
@@ -1,15 +1,29 @@
 import { TPlayer, getDateByEpoch, getPlayers } from '@Utils'
 import { useQuery } from '@tanstack/react-query'
 
-/** ---------------- @Images_Icons ------------------- */
-import externalLink from '@Assets/externalLink.png'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+/** ---------------- @Images_Icons ------------------- */
+import externalLink from '@Assets/externalLink.png'
+import sort from '@Assets/sort.png'
+
+interface ISortOrder {
+  Age: 'asc' | 'desc'
+  Name: 'asc' | 'desc'
+  Rank: 'asc' | 'desc'
+}
 
 const Table = () => {
   const navigate = useNavigate()
 
-  const tableHeader = ['Name', 'Type', 'Points', 'Rank', 'Age']
+  const tableHeader = ['Name', 'Rank', 'Age', 'Type', 'Points']
+  const isFilterableColumn = ['Name', 'Age', 'Rank']
+
+  const [sortOrder, setSortOrder] = useState<ISortOrder>({
+    Age: 'asc',
+    Name: 'asc',
+    Rank: 'asc',
+  })
 
   const [cricketerList, setCricketerList] = useState<TPlayer[]>([])
   const [setsearchByName, setSearchByName] = useState<string>('')
@@ -44,6 +58,47 @@ const Table = () => {
     }
   }
 
+  const sortListByColumnType = (columnType: string) => {
+    if (columnType === 'Age') {
+      setCricketerList((prevState) =>
+        sortOrder.Age === 'asc'
+          ? [...prevState.sort((a, b) => (a.dob ?? 0) - (b.dob ?? 0))]
+          : [...prevState.sort((a, b) => (b.dob ?? 0) - (a.dob ?? 0))],
+      )
+      setSortOrder((prevState) => ({
+        ...prevState,
+        Age: prevState['Age'] === 'asc' ? 'desc' : 'asc',
+      }))
+    }
+    if (columnType === 'Name') {
+      setCricketerList((prevState) => [
+        ...prevState.sort((a, b) => {
+          if ((a.name ?? '') < (b.name ?? '')) {
+            return sortOrder.Name === 'asc' ? 1 : -1
+          }
+          if ((a.name ?? '') > (b.name ?? '')) {
+            return sortOrder.Name === 'asc' ? -1 : 1
+          }
+          return 0
+        }),
+      ])
+      setSortOrder((prevState) => ({
+        ...prevState,
+        Name: prevState['Name'] === 'asc' ? 'desc' : 'asc',
+      }))
+    }
+    if (columnType === 'Rank') {
+      setCricketerList((prevState) =>
+        sortOrder.Rank === 'asc'
+          ? [...prevState.sort((a, b) => (a.rank ?? 0) - (b.rank ?? 0))]
+          : [...prevState.sort((a, b) => (b.rank ?? 0) - (a.rank ?? 0))],
+      )
+      setSortOrder((prevState) => ({
+        ...prevState,
+        Rank: prevState['Rank'] === 'asc' ? 'desc' : 'asc',
+      }))
+    }
+  }
   return (
     <div className="flex flex-col">
       <div className="rounded relative h-96 overflow-y-auto">
@@ -56,7 +111,26 @@ const Table = () => {
                   scope="col"
                   className="px-6 py-3 text-textColor"
                 >
-                  {heading}
+                  <div
+                    className={`flex items-center ${
+                      isFilterableColumn.includes(heading)
+                        ? 'cursor-pointer select-none'
+                        : ''
+                    }`}
+                    onClick={() =>
+                      isFilterableColumn.includes(heading) &&
+                      sortListByColumnType(heading)
+                    }
+                  >
+                    <p>{heading}</p>
+                    {isFilterableColumn.includes(heading) && (
+                      <img
+                        src={sort}
+                        alt="sort-item"
+                        className="icon-color object-contain h-3 ml-3"
+                      />
+                    )}
+                  </div>
                 </th>
               ))}
             </tr>
@@ -103,16 +177,16 @@ const Table = () => {
                     </div>
                   </th>
                   <td className="px-6 py-4 text-textColor">
-                    {playerDetails?.type ?? '-'}
-                  </td>
-                  <td className="px-6 py-4 text-textColor">
-                    {playerDetails?.points ?? '-'}
-                  </td>
-                  <td className="px-6 py-4 text-textColor">
                     {playerDetails?.rank ?? '-'}
                   </td>
                   <td className="px-6 py-4 text-textColor">
                     {getDateByEpoch(playerDetails?.dob ?? 0)}
+                  </td>
+                  <td className="px-6 py-4 text-textColor">
+                    {playerDetails?.type ?? '-'}
+                  </td>
+                  <td className="px-6 py-4 text-textColor">
+                    {playerDetails?.points ?? '-'}
                   </td>
                 </tr>
               ))}
